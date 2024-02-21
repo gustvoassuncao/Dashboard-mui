3b6437965faa3fd27fe126bb4571b5e476202c67
Gustavo Assunção <104602579+gustvoassuncao@users.noreply.github.com>
2024-02-21 18:04:46 -0300

    [2]: React Router DOM installation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,11 @@
 // Esse aqui vai ser responsável por quando dar um export * para tudo
+import { Routes, Route, Navigate } from "react-router-dom";
 
-export {};
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/home-page" element={<p>Página Inicial</p>} />
+            <Route path="*" element={<Navigate to="home-page"/>} />
+        </Routes>
+)
+};
